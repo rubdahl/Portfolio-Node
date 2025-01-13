cbe9038f5efe681c2960e5c217cca471e9573e74
Ruben Dahl <dahlrub1@gmail.com>
2025-01-13 10:39:59 +0100

    add jquery

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use('/', index);
 app.use('/users', users);
